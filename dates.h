2ce939cbbece31f362344aeef358ac3c846e6384
BecaudA <becaud.arthur@gmail.com>
2018-12-04 14:29:59 +0100

    Revert "Revert "Edit SaisieDate& Affichage""
    This reverts commit e03775ba08cc2c9948f6bbc15fc232f23f0e3765.

--- dates.h
+++ dates.h
@@ -1,8 +1,17 @@
 #ifndef LABO_09_NBREJOURS_DATES_H
 #define LABO_09_NBREJOURS_DATES_H
 
-
-bool saisieDate(int& jours, int& mois, int& annee, const char& CAR);
+/**
+ * @brief
+ * @param jours
+ * @param mois
+ * @param annee
+ * @param CAR
+ * @param DATE_MIN
+ * @param DATE_MAX
+ * @return
+ */
+bool saisieDate(int& jours, int& mois, int& annee, const char& CAR, const int DATE_MIN[], const int DATE_MAX[]);
 
 bool validationDate(const int DATE[3]);
 
@@ -17,5 +26,7 @@ bool plusPetiteOuEgale(const int DATE[3], const int DATE_REF[3]);
 bool plusGrandeOuEgale(const int DATE[3], const int DATE_REF[3]);
 
 int differenceDate(const int DATE1[3], const int DATE2[3]);
+
+void afficherDate(const int DATE[3], const char& CAR);
 
 #endif //LABO_09_NBREJOURS_DATES_H
